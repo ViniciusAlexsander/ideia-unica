6b235014ff1963f2acbb5a0831a99da69013d12a
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2021-01-21 21:49:41 -0300

    testando revalida]]ao

---
+++ pages/tempo.js
@@ -0,0 +1,27 @@
+function Tempo(props) {
+  const dynamicDate = new Date();
+  const dynamicDateString = dynamicDate.toGMTString();
+
+
+  return (
+    <div>
+      <div> {dynamicDateString} (dinâmico)</div>
+      <div> {props.staticDateString} (estático)</div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const staticDate = new Date();
+  const staticDateString = staticDate.toGMTString();
+
+  return {
+    props: {
+      staticDateString
+    },
+    //para revalidar os dados a cada 1 segundo
+    revalidate: 1
+  }
+}
+
+export default Tempo;
